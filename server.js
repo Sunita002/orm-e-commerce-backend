4d7dd5e0fd83d356bb9c5be0d5d08e667302eedf
sunita persad <sunitapersad020@gmail.com>
2021-11-21 19:54:37 -0500

    use routes and app.listen

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync Sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
+});
